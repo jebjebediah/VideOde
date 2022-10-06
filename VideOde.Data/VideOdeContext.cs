d74ad951efaef14dfd68d69f8bcc890e3a700c74
Jeb Bishop <jeb@jeb.zone>
2022-10-05 20:57:33 -0400

    Clip data accessor

--- VideOde.Data/VideOdeContext.cs
+++ VideOde.Data/VideOdeContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VideOde.Core;
 
 namespace VideOde.Data;
 public class VideOdeContext : DbContext
 {
+    public DbSet<Clip> Clips { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite($"Data Source={Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\VideOde\\VideOde.db");

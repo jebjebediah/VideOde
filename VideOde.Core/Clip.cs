a375f3c112215e5a64659b6b01da9c24148d3ceb
Jeb Bishop <jeb@jeb.zone>
2022-10-05 20:56:31 -0400

    Adding Clip data class

---
+++ VideOde.Core/Clip.cs
@@ -0,0 +1,12 @@
+﻿namespace VideOde.Core
+{
+    public class Clip
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int Length { get; set; }
+        public string? Description { get; set; }
+    }
+}
